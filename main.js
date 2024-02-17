b2404e437227dfb1f77a4d8ea81a48c8a4c0f877
DaniaHaija <98768902+DaniaHaija@users.noreply.github.com>
2024-02-17 11:21:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector(".click2").onclick=function(){
+    document.querySelector(".right-side").classList.remove("d-none");
+}
+document.querySelector(".click1").onclick=function(){
+    document.querySelector(".right-side").classList.add("d-none");
+}
